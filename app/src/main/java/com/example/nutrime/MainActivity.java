1624f9a0f822de18bd5963cb050c681251006bea
Tabea Stremayr <tabea-maria.stremayr@edu.fh-joanneum.at>
2022-12-14 20:04:27 +0100

    add rudimentary main screen

--- app/src/main/java/com/example/nutrime/MainActivity.java
+++ app/src/main/java/com/example/nutrime/MainActivity.java
@@ -1,13 +1,21 @@
 package com.example.nutrime;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
+import com.example.nutrime.R;
+
+import com.google.android.material.button.MaterialButton;
+
 
 public class MainActivity extends AppCompatActivity {
+
+    private boolean isHidden = true;
+    private int searchWindowDifference = 1430;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,5 +26,15 @@ public class MainActivity extends AppCompatActivity {
     public void switchScreen(View view){
         Intent intent = new Intent(MainActivity.this, SecondActivity.class);
         startActivity(intent);
+    }
+
+    public void switchSearch(View view) {
+        if (isHidden) {
+            findViewById(R.id.fragmentContainerView4).setY(findViewById(R.id.fragmentContainerView4).getY() + searchWindowDifference);
+        }
+        else {
+            findViewById(R.id.fragmentContainerView4).setY(findViewById(R.id.fragmentContainerView4).getY() - searchWindowDifference);
+        }
+        isHidden = !isHidden;
     }
 }
---
+++ app/src/main/java/com/example/nutrime/page2.java
@@ -0,0 +1,36 @@
+package com.example.nutrime;
+
+import androidx.lifecycle.ViewModelProvider;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class page2 extends Fragment {
+
+    private Page2ViewModel mViewModel;
+
+    public static page2 newInstance() {
+        return new page2();
+    }
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_page2, container, false);
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        mViewModel = new ViewModelProvider(this).get(Page2ViewModel.class);
+        // TODO: Use the ViewModel
+    }
+
+}
